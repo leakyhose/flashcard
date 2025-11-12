dc456a08067bfa187b31f7c0def8edf7dc629088
leakyhose <yimingsu2007@gmail.com>
2025-11-12 13:02:59 -0800

    Add serverHelpers.ts, implemented lobby code generateion

--- backend/src/serverHelpers.ts
+++ backend/src/serverHelpers.ts
@@ -1,4 +1,13 @@
 import type { Lobby, Player, Flashcard } from "@shared/types.js"
 
-function createLobby(){
+function generateCode(lenght = 4){
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let code = "";
+
+    for(let i = 0; i < 4; i ++){
+        code += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+
+    return code;
 }
+
