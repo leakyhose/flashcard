a34b38da82bfde90b5318ca217c7630e6c8d4537
unknown <yimingsu2007@gmail.com>
2025-11-16 00:01:03 -0800

    Run prettier

--- frontend/src/components/Players.tsx
+++ frontend/src/components/Players.tsx
@@ -17,9 +17,12 @@ export function Players({ players }: PlayersProps) {
     <div className="flex-1 flex flex-col overflow-hidden">
       <ul className="flex-1 overflow-auto border border-grey-100">
         {players.map((player) => (
-          <li key={player.id} className="border border-grey-100 flex w-full group relative">
-            <div 
-              className={`flex w-full ${isLeader && player.id != socket.id ? 'cursor-pointer group-hover:opacity-0 transition-opacity' : ''}`}
+          <li
+            key={player.id}
+            className="border border-grey-100 flex w-full group relative"
+          >
+            <div
+              className={`flex w-full ${isLeader && player.id != socket.id ? "cursor-pointer group-hover:opacity-0 transition-opacity" : ""}`}
               onClick={() => isLeader && handleUpdateLeader(player.id)}
             >
               <div className="truncate basis-[70%] shrink p-3">
